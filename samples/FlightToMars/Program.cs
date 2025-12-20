42f69cc558fbca2b9d2f6dd8be58b8fd47f5b8dc
NikitaDudinov <dudinovnik@yandex.ru>
2025-12-20 11:29:00 +0300

    Добавил примеры с Live Coding

---
+++ samples/FlightToMars/Program.cs
@@ -0,0 +1,63 @@
+﻿//Мы находимся в Центре Управления Полетами.
+//Наша задача — запустить две ракеты.
+//Одна летит на Луну, другая — на Марс.
+//Раньше инженеры писали код отдельно для каждой ракеты, дублируя работу,
+//и это было долго и неудобно.
+
+//Твоя задача — создать единый чертеж Ракеты, то есть Класс.
+//Запомни, что любая ракета должна иметь имя, запас топлива и текущую высоту полета.
+//Далее сделай так, чтобы ракеты умели летать, написав специальный метод Fly.
+//Когда этот метод вызывается, ракета должна тратить топливо и подниматься выше.
+//В финале, в главном центре управления, создай две разные ракеты по этому чертежу и
+//запусти их одновременно в цикле, пока у них не кончится топливо.
+
+
+Rocket rocket = new Rocket("Союз", 100, 0);
+Rocket rocket1 = new Rocket("Спутник", 80, 0);
+
+while (rocket.GetFuel() > 0 || rocket1.GetFuel() > 0)
+{
+    rocket.Fly();
+    rocket1.Fly();
+}
+
+Console.WriteLine("Высота ракеты " + rocket.GetName() + " " + rocket.GetHeight());
+Console.WriteLine("Высота ракеты " + rocket1.GetName() + " " + rocket1.GetHeight());
+
+class Rocket
+{
+    private string _name;
+    private int _fuel;
+    private int _height;
+
+    public Rocket(string name, int fuel, int height)
+    {
+        _name = name;
+        _fuel = fuel;
+        _height = height;
+    }
+
+    public void Fly()
+    {
+        if(_fuel > 0)
+        {
+            _fuel -= 10;
+            _height += 50;
+        }
+    }
+
+    public int GetFuel()
+    {
+        return _fuel;
+    }
+
+    public int GetHeight()
+    {
+        return _height;
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+}
---
+++ samples/Pet/Program.cs
@@ -0,0 +1,108 @@
+﻿//Ты — смотритель космической станции, обнаруживший неизвестную форму жизни.
+//Твоя задача — создать программную модель этого организма, обладающего уникальным Именем
+//и жизненно важными показателями: Здоровьем, Голодом и запасом Энергии. 
+//Тебе предстоит реализовать систему команд, позволяющую кормить питомца для утоления голода, играть с ним, расходуя его силы,
+//и укладывать спать для полного восстановления. Программа должна работать в непрерывном цикле, обновляя и показывая состояние
+//существа после каждого действия, ведь если его здоровье опустится до нуля, миссия будет считаться проваленной.
+
+Pet pet = new Pet("животное", 100, 30, 70);
+Console.WriteLine("Начальное состояние: ");
+pet.DisplayState();
+
+while (true)
+{
+    string command = Console.ReadLine();
+    if(command == "feed")
+    {
+        pet.ToFeed();
+    }
+    else if(command == "play")
+    {
+        pet.ToPlay();
+    }
+    else if (command == "sleep")
+    {
+        pet.ToSleep();
+    }
+    else if(command == "exit")
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Unknow command");
+    }
+    pet.DisplayState();
+}
+Console.WriteLine("Вышли из программы");
+
+class Pet
+{
+    private string _name;
+    private int _health;
+    private int _hunger;
+    private int _energy;
+
+    public Pet(string name,  int health, int hunger, int energy)
+    {
+        _name = name;
+        _health = health;
+        _hunger = hunger;
+        _energy = energy;
+    }
+
+    public void ToSleep()
+    {
+        Console.WriteLine(_name + " лег спать");
+        _health = 100;
+        _hunger = 0;
+        _energy = 100;
+    }
+
+    public void ToPlay()
+    {
+        Console.WriteLine(_name + " играет");
+        if (_energy > 0)
+        {
+            _energy -= 10;
+        }
+    }
+
+    public void ToFeed()
+    {
+        Console.WriteLine(_name + " кушает");
+        if (_hunger > 0) 
+        {
+            _hunger -= 10;
+        }
+    }
+
+    public void DisplayState()
+    {
+        Console.WriteLine("Имя: " + _name);
+        Console.WriteLine("Здоровье: " + _health);
+        Console.WriteLine("Голод: " + _hunger);
+        Console.WriteLine("Энергия: " + _energy);
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public int GetHealth()
+    {
+        return _health;
+    }
+
+    public int GetHunger() 
+    {
+        return _hunger;
+    }
+
+    public int GetEnergy() 
+    {
+        return _energy;
+    }
+}
+
